910b853cd85144ca1c208b103b30a7e045eb2c94
Diego Aguilera <diegoaguilerareyes4@gmail.com>
2023-04-08 21:16:48 -0400

    agregación de librería grafica

--- Code/cliente/src/components/TablaMovimientos.vue
+++ Code/cliente/src/components/TablaMovimientos.vue
@@ -7,7 +7,7 @@
       :search="search"
       :sort-by="sortBy.toLowerCase()"
       :sort-desc="sortDesc"
-      hide-default-footer
+      :footer-props="{'items-per-page-text':'Alertas por página'}"
     >
       <template v-slot:header>
         <v-toolbar dark color="blue darken-3" class="mb-1">
@@ -18,7 +18,7 @@
             solo-inverted
             hide-details
             prepend-inner-icon="mdi-magnify"
-            label="Search"
+            label="Buscar"
           ></v-text-field>
           <template v-if="$vuetify.breakpoint.mdAndUp">
             <v-spacer></v-spacer>
@@ -28,8 +28,9 @@
               solo-inverted
               hide-details
               :items="keys"
+              clearable
               prepend-inner-icon="mdi-magnify"
-              label="Sort by"
+              label="Ordenar por"
             ></v-select>
             <v-spacer></v-spacer>
             <v-btn-toggle v-model="sortDesc" mandatory>
@@ -81,53 +82,7 @@
         </v-row>
       </template>
 
-      <template v-slot:footer>
-        <v-row class="mt-2" align="center" justify="center">
-          <span class="grey--text">Items per page</span>
-          <v-menu offset-y>
-            <template v-slot:activator="{ on, attrs }">
-              <v-btn
-                dark
-                text
-                color="primary"
-                class="ml-2"
-                v-bind="attrs"
-                v-on="on"
-              >
-                {{ itemsPerPage }}
-                <v-icon>mdi-chevron-down</v-icon>
-              </v-btn>
-            </template>
-            <v-list>
-              <v-list-item
-                v-for="(number, index) in itemsPerPageArray"
-                :key="index"
-                @click="updateItemsPerPage(number)"
-              >
-                <v-list-item-title>{{ number }}</v-list-item-title>
-              </v-list-item>
-            </v-list>
-          </v-menu>
-
-          <v-spacer></v-spacer>
-
-          <span class="mr-4 grey--text">
-            Page {{ page }} of {{ numberOfPages }}
-          </span>
-          <v-btn
-            fab
-            dark
-            color="blue darken-3"
-            class="mr-1"
-            @click="formerPage"
-          >
-            <v-icon>mdi-chevron-left</v-icon>
-          </v-btn>
-          <v-btn fab dark color="blue darken-3" class="ml-1" @click="nextPage">
-            <v-icon>mdi-chevron-right</v-icon>
-          </v-btn>
-        </v-row>
-      </template>
+      
     </v-data-iterator>
   </v-container>
 </template>
